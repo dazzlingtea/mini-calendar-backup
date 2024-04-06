a6b9e026141fbd743a2eed83c072c4086f150987
dazzlingtea <kitsnr08@gmail.com>
2024-04-07 00:28:22 +0900

    Feat: add function rendering dates of Calendar

--- calendar.js
+++ calendar.js
@@ -5,9 +5,11 @@ let date = new Date();
 const isNotCurrMonth = (dates) => {
   const $dateContainer = document.querySelector('.date-container');
   dates.forEach((date, index) => {
-    console.log(`${date}`);
-    if( !date.currentMonth ) {
-      $dateContainer.querySelector(`span[data-date-idx="${index}"]`).classList.add('inactive');
+    const spanElement = $dateContainer.querySelector(`span[data-date-idx="${index}"]`);
+    if (!date.currentMonth) {
+      spanElement.classList.add('inactive');
+      console.log(spanElement);
+      console.log(date);
     }
   });    
 }
@@ -15,7 +17,7 @@ const isNotCurrMonth = (dates) => {
 const renderCalendar = () => {
   // 오늘 기준 연도, 월 구하기
   const viewYear = date.getFullYear();
-  const viewMonth = date.getMonth();
+  const viewMonth = date.getMonth() - 2;
   
   // 캘린더 헤더 제목 오늘 기준으로 변경
   document.querySelector('.year-month').textContent = `${viewYear}년 ${viewMonth + 1}월`;
@@ -38,14 +40,14 @@ const renderCalendar = () => {
   // 지난 달 마지막 주 + 이번 달 전체 + 다음 달 첫 주
   // 이번 달 1월이 아니면 그대로, 1월이면 연도-1년 12월
   const prevDates = Array
-  .from({ length: prevLastDay + 1 }, 
-    (_, index) => ({ 
-      year: viewMonth !== 0 ? viewYear : viewYear - 1,
-      month: viewMonth !== 0 ? viewMonth : viewMonth + 12,
-      date: prevLastDate - prevLastDay + index,
-      currentMonth: false,
-    }));
-    const currDates = Array
+    .from({ length: prevLastDay + 1 }, 
+      (_, index) => ({ 
+        year: viewMonth !== 0 ? viewYear : viewYear - 1,
+        month: viewMonth !== 0 ? viewMonth : viewMonth + 12,
+        date: prevLastDate - prevLastDay + index,
+        currentMonth: false,
+      }));
+  const currDates = Array
     .from({ length: currLastDate }, 
       (_, index) => ({
         year: viewYear,
@@ -54,7 +56,7 @@ const renderCalendar = () => {
         currentMonth: true,
       }));
       // 이번 달 12월이 아니면 그대로, 12월이면 연도+1년 1월
-      const nextDates = Array
+  const nextDates = Array
       .from({ length: 42 - prevDates.length - currDates.length }, 
         (_, index) => ({
           year: viewMonth + 1 !== 12 ? viewYear : viewYear + 1,
@@ -63,21 +65,20 @@ const renderCalendar = () => {
           currentMonth: false,
         }));
         
-        // 배열 합치기 : 지난 달 마지막 주 + 이번 달 전체 + 다음 달 첫 주
-        const dates = [...prevDates, ...currDates, ...nextDates];
+  // 배열 합치기 : 지난 달 마지막 주 + 이번 달 전체 + 다음 달 첫 주
+  const dates = [...prevDates, ...currDates, ...nextDates];
         
   // Dates 태그 형태로 정리
-  dates.forEach((date, i) => {
-    dates[i] = `<div class="date-box"><span class="date-text" data-date-idx="${i}">${date.date}</span></div>`;
-    date.id = i;
-    console.log(date);
+  const tagDates = dates.map((date, i) => {
+    date.id = i; // 원본 배열의 객체마다 id 추가
+    return `<div class="date-box"><span class="date-text" data-date-idx="${i}">${date.date}</span></div>`;
   })
 
   // Dates 화면 렌더링
-  document.querySelector('.date-container').innerHTML = dates.join('');
+  document.querySelector('.date-container').innerHTML = tagDates.join('');
   
-  isNotCurrMonth(dates, viewMonth);
   // 이번 달이 아닌 날짜 흐리게 변경 클래스 추가
+  isNotCurrMonth(dates);
 
 }
 
