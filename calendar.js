4578efdb36ee7fdae3f81fd1c2da9de487db0f27
dazzlingtea <kitsnr08@gmail.com>
2024-04-07 01:06:24 +0900

    Feat add function appending inactive class to span element

--- calendar.js
+++ calendar.js
@@ -2,24 +2,29 @@ let date = new Date();
 
 //=====함수 정의 =====//
 
-const isNotCurrMonth = (dates) => {
+// inactive 클래스 추가하는 함수 
+// 캘린더에 표시된 이번달 = 지난달 말 + 이번달 + 다음달 초 (dates 배열)  
+const addInactiveClass = (dates) => {
+  // .date-container 요소 후손 중에서 찾기
   const $dateContainer = document.querySelector('.date-container');
+  // iterate  date 요소)
   dates.forEach((date, index) => {
-    const spanElement = $dateContainer.querySelector(`span[data-date-idx="${index}"]`);
+    // currentMonth: false인 span 태그에 inactive 클래스 추가
     if (!date.currentMonth) {
-      spanElement.classList.add('inactive');
-      console.log(spanElement);
-      console.log(date);
+      const $spanDateText = $dateContainer.querySelector(`span[data-date-idx="${index}"]`);
+      $spanDateText.classList.add('inactive');
     }
   });    
 }
 
-const renderCalendar = () => {
-  // 오늘 기준 연도, 월 구하기
+// 캘린더 뷰에서 표시될 날짜 구하는 함수 (6주 * 7요일 = 총 42칸)
+const renderCalendarView = () => {
+  // 캘린더 뷰 기본값은 현재시각 기준 연월
   const viewYear = date.getFullYear();
-  const viewMonth = date.getMonth() - 2;
+  const viewMonth = date.getMonth();
   
-  // 캘린더 헤더 제목 오늘 기준으로 변경
+  // 캘린더 헤더 제목은 뷰 기준으로 변경
+  // 버튼 고려해서 수정할 예정
   document.querySelector('.year-month').textContent = `${viewYear}년 ${viewMonth + 1}월`;
   
   // 지난 달 마지막 날, 이번 달 마지막 날
@@ -28,7 +33,7 @@ const renderCalendar = () => {
   const prevLast = new Date(viewYear, viewMonth, 0);
   const currLast = new Date(viewYear, viewMonth + 1, 0);
   
-  // 이전 달 마지막 날짜와 요일,
+  // 지난 달 마지막 날짜와 요일,
   // 이번 달 마지막 날짜 구하기
   const prevLastDate = prevLast.getDate();
   const prevLastDay = prevLast.getDay();
@@ -36,8 +41,7 @@ const renderCalendar = () => {
   const currLastDate = currLast.getDate();
   // const currLastDay = currLast.getDay();
   
-  // 이번 달 달력에서 표시될 날짜 구하기 6주 * 7요일
-  // 지난 달 마지막 주 + 이번 달 전체 + 다음 달 첫 주
+  // 지난 달 말 + 이번 달 전체 + 다음 달 초
   // 이번 달 1월이 아니면 그대로, 1월이면 연도-1년 12월
   const prevDates = Array
     .from({ length: prevLastDay + 1 }, 
@@ -55,7 +59,7 @@ const renderCalendar = () => {
         date: index + 1,
         currentMonth: true,
       }));
-      // 이번 달 12월이 아니면 그대로, 12월이면 연도+1년 1월
+  // 이번 달 12월이 아니면 그대로, 12월이면 연도+1년 1월
   const nextDates = Array
       .from({ length: 42 - prevDates.length - currDates.length }, 
         (_, index) => ({
@@ -65,7 +69,7 @@ const renderCalendar = () => {
           currentMonth: false,
         }));
         
-  // 배열 합치기 : 지난 달 마지막 주 + 이번 달 전체 + 다음 달 첫 주
+  // 배열 합치기 : 지난 달 말 + 이번 달 전체 + 다음 달 초
   const dates = [...prevDates, ...currDates, ...nextDates];
         
   // Dates 태그 형태로 정리
@@ -77,8 +81,8 @@ const renderCalendar = () => {
   // Dates 화면 렌더링
   document.querySelector('.date-container').innerHTML = tagDates.join('');
   
-  // 이번 달이 아닌 날짜 흐리게 변경 클래스 추가
-  isNotCurrMonth(dates);
+  // 이번 달이 아닌 날짜만 흐리게 스타일 변경하는 클래스 추가
+  addInactiveClass(dates);
 
 }
 
@@ -94,4 +98,4 @@ const renderCalendar = () => {
 
 //=== 함수 실행 영역
 
-renderCalendar();
+renderCalendarView();
